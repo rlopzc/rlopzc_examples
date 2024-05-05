1f4d3ce725844e8664b4217432e1728a3d0a4a1c
Romario López <romariolopezc@gmail.com>
2024-05-04 20:39:05 -0600

    initial commit

---
+++ lib/rlopzc.ex
@@ -0,0 +1,18 @@
+defmodule Rlopzc do
+  @moduledoc """
+  Documentation for `Rlopzc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rlopzc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rlopzc_test.exs
@@ -0,0 +1,8 @@
+defmodule RlopzcTest do
+  use ExUnit.Case
+  doctest Rlopzc
+
+  test "greets the world" do
+    assert Rlopzc.hello() == :world
+  end
+end
